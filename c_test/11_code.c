c6993516e876acdb45f2edfa7e0dbef6501e192c
Yao Jie <yaoojie@foxmail.com>
2023-07-17 09:34:52 +0800

    asd

--- c_test/11_code.c
+++ c_test/11_code.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(int arvc, char *argv[]) {
-    printf("Hello World\n");
+
+    int arr[3][4] = {{1, 2}, {5, 6}, {9, 10, 11}};
+
+    int m = sizeof(arr) / sizeof(arr[0]);
+    int n = sizeof(arr[0]) / sizeof(arr[0][0]);
+
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            printf("%d", arr[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
