914b0c1453dac3189f18c83c8bf29e71b142a664
Yao Jie <yaoojie@foxmail.com>
2023-07-21 09:22:59 +0800

    update

--- LicensePlateRecognition/LicensePlateRecognition2.cpp
+++ LicensePlateRecognition/LicensePlateRecognition2.cpp
@@ -8,20 +8,11 @@ const static std::string request_url =
 
 static std::string licensePlate_result;
 
-/**
- * curl发送http请求调用的回调函数，回调函数中对返回的json格式的body进行了解析，解析结果储存在全局的静态变量当中
- * @param 参数定义见libcurl文档
- * @return 返回值定义见libcurl文档
- */
 static size_t callback(void *ptr, size_t size, size_t nmemb, void *stream) {
-    // 获取到的body存放在ptr中，先将其转换为string格式
     licensePlate_result = std::string((char *)ptr, size * nmemb);
     return size * nmemb;
 }
-/**
- * 车牌识别
- * @return 调用成功返回0，发生错误返回其他错误码
- */
+
 int licensePlate(std::string &json_result, const std::string &access_token) {
     std::string url = request_url + "?access_token=" + access_token;
     CURL *curl = NULL;
@@ -56,7 +47,7 @@ int licensePlate(std::string &json_result, const std::string &access_token) {
 }
 
 int main(int argc, char *argv[]) {
-    // 设置APPID/AK/SK
+
     std::string app_id = "36423029";
     std::string api_key = "o1ybLKZQRYIGYniwEHW3DYaO";
     std::string secret_key = "v5I3fV0CI9hFRl9IqmPOm3aaEiSFy9dv";
