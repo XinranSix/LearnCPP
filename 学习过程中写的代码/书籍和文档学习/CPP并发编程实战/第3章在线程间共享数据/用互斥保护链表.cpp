4328d75df89b2668155e81120a11d11d9503eb53
Yao Jie <yaoojie@foxmail.com>
2023-08-26 17:59:33 +0800

    备份

---
+++ 学习过程中写的代码/书籍和文档学习/CPP并发编程实战/第3章在线程间共享数据/用互斥保护链表.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <mutex>
+
+std::list<int> some_list;
+std::mutex some_mutex;
+
+void add_to_list(int new_value) {
+    std::lock_guard<std::mutex> guard { some_mutex };
+    some_list.push_back(new_value);
+}
+
+bool list_contains(int value_to_find) {
+    std::lock_guard<std::mutex> guard { some_mutex };
+    return std::find(begin(some_list), end(some_list), value_to_find) != end(some_list);
+}
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
