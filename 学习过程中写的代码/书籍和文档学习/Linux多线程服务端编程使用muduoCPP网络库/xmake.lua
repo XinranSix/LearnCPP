7dcd1f9fed52f24dafdfec4a0277995016d3c952
Yao Jie <yaoojie@foxmail.com>
2023-09-01 14:45:59 +0800

    Linux多线程服务端编程使用muduoCPP网络库😎

---
+++ 学习过程中写的代码/书籍和文档学习/Linux多线程服务端编程使用muduoCPP网络库/xmake.lua
@@ -0,0 +1,8 @@
+add_includedirs("./include")
+
+for _, filepath in ipairs(os.files("./**.cpp")) do
+target('Linux多线程服务端编程使用muduoCPP网络库_' .. path.basename(filepath))
+    add_files(filepath)
+    set_rundir(path.directory(filepath))
+    add_packages("fmt", "boost", "range-v3", "gtest", "magic_enum", "cpprest")
+end
